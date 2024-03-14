52da1e8db7bcd0909abe48b139b2d4f7035847a6
Marco <professormarco77@gmail.com>
2024-03-14 01:03:44 +0100

    work on landingPage

--- website/static/navbarScript.js
+++ website/static/navbarScript.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () { //This wrapper makes sure, the document is fully loaded before code is run
   document.getElementById("logo").addEventListener("click", toHome)
   document.getElementById("about-me-link").addEventListener("click", toAboutMe);
   document.getElementById("projects-link").addEventListener("click", toProjects);
