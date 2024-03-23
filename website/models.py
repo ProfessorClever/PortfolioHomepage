1a361592f5f1b13ce4d1921b70c3b68260f452fd
Marco <professormarco77@gmail.com>
2024-03-23 10:45:38 +0100

    work on models.py

--- website/models.py
+++ website/models.py
@@ -3,18 +3,20 @@ from website import db
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    filename = db.Column(db.String, unique=True)
+    
+    filename = db.Column(db.String, nullable=False, unique=True)
 
 class Text(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String)
+    textType = db.Column(db.String)
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, unique=True)
-    content_id = db.Column(db.Integer, foreign_key=True)
+    discription = db.Column(db.String)
 
 class ProjectElement(db.Model):
-    project_id = db.Column(db.Integer, foreign_key=True, primary_key=True)
-    element_id = db.Column(db.Integer, foreign_key=True, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey("project.id"), primary_key=True)
+    element_id = db.Column(db.Integer, db.ForeignKey("project.id"), primary_key=True)
     position = db.Column(db.Integer)
