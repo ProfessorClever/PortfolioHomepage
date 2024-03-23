ecc5c1aabaa9c092bc1787d754d3e991977da858
Marco <professormarco77@gmail.com>
2024-03-23 08:35:59 +0100

    add db

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+#App inizialisierung
 app = Flask(__name__)
+
+#Rufe configuration auf
+app.config.from_object('config')
+
+#Datenbank initialisierung
+db = SQLAlchemy()
+db.init_app(app)
 
 from website import routes
