8476222bbab0b6ad39e168a59dea84a8b0deaa80
Marco <133157182+ProfessorClever@users.noreply.github.com>
2024-08-11 00:50:46 +0200

    work on index and project cards, add default image svg

--- website/static/indexScript.js
+++ website/static/indexScript.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("knowMeButton").addEventListener("click", toAboutMe);
     document.getElementById("showProjectsButton").addEventListener("click", toProjects);
+    document.getElementById("latest").addEventListener("click", () => {
+        const latest_id = document.getElementById("latest_id").innerText
+        window.location.assign('/Projects/'+latest_id)
+    });
+    document.getElementById("popular").addEventListener("click", () => {
+        const popular_id = document.getElementById("popular_id").innerText
+        window.location.assign('/Projects/'+popular_id)
+    });
 });
